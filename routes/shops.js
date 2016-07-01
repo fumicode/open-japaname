5622b074107f52d8621d274878e2c8f11485a9ea
udonka <icymasa@gmail.com>
2016-07-01 22:11:23 +0000

    added width parameter.

--- routes/shops.js
+++ routes/shops.js
@@ -18,6 +18,8 @@ shops_router.get("/:shopname/artworks/:artwork_name/:japaname_code",(req,res,nex
     var artwork_name = req.params.artwork_name;
     var japaname_code = req.params.japaname_code;
 
+    var artwork_width =  req.query.artwork_width || 50;
+
     if(shopname == "arton"){  //haveRight = authModule.authorize("arton")
       var ofuda = "risumaru_ofuda";
       // var ino   = "risumaru_ino"; var shika = "risumaru_shika"; var chou  = "risumaru_chou"; 
@@ -36,9 +38,11 @@ shops_router.get("/:shopname/artworks/:artwork_name/:japaname_code",(req,res,nex
           artwork_name:ofuda,
           japaname_id:japaname.id
         });
+console.log("artwork");
+console.log(artwork_width);
 
         if(purchaseExists){ //すでに購入していた
-          return res.render("artworks/risumaru_ofuda" , {japaname});
+          return res.render("artworks/" + artwork_name, {japaname, artwork_width});
         }
         else{
           return res.redirect(path.join(req.baseUrl , req.url , "/preview"));
