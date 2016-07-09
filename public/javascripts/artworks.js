c0e66d1976c826499f575b1002f91023ae8662b2
udonka <icymasa@gmail.com>
2016-07-10 02:46:59 +0900

    added char size adjustment.

--- public/javascripts/artworks.js
+++ public/javascripts/artworks.js
@@ -7,6 +7,9 @@
       var svg = Snap(request.responseXML.documentElement)
         .attr({width:"100%", height:"100%"});
 
+
+
+
       Object.keys(variables).forEach(function(fieldName){
         var fieldValue = variables[fieldName];
         var element = svg.paper.select("#"+ fieldName);
@@ -14,16 +17,53 @@
         if(element){
           element.attr("text", fieldValue);
 
-          if(fieldName == "japaname_string"){
-            //var length = element.textLength();
+        }
+
+        if(fieldName == "japaname_string"){
+
+          var japaname_string = fieldValue;
+
+
+          var japaname_box = svg.paper.select("#japaname_box");
+
+
+          var x = parseInt(japaname_box.attr("x"));
+          var y = parseInt(japaname_box.attr("y"));
+          var box_height= parseInt(japaname_box.attr("height"));
+
+
+          if(!japaname_box){
+            alert("japaname_box not defined")
+            return;
           }
+
+          var box_height = 125;
+          var char_num =  japaname_string.length;
+
+          var char_height = box_height/ char_num;
+
+
+          for(index  in  japaname_string){
+            var chara = japaname_string[index];
+
+            var text = japaname_box.text(x, y + char_height/2 + (index ) * char_height, chara);
+
+            text.attr({style:'font-family:"ro-nikkatsu-sei-kai-std";font-size:'+char_height+'px;glyph-orientation-vertical:0;writing-mode:tb;fill:#FFFFFF', "text-anchor":"middle"})
+
+            
+          }
+
+
+
         }
       });
 
 
-      var image_element = svg.paper.select("#qrcode");
-      if(image_element){
-        image_element.attr("xlink:href", variables.qrcode_url);
+      {
+        var image_element = svg.paper.select("#qrcode");
+        if(image_element){
+          image_element.attr("xlink:href", variables.qrcode_url);
+        }
       }
 
       svg.prependTo(container);
