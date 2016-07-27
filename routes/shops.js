8413cfd20dd18e203431ade7bc0ab7177ef24142
udonka <icymasa@gmail.com>
2016-07-27 18:26:00 +0900

    added arton page.

--- routes/shops.js
+++ routes/shops.js
@@ -5,22 +5,35 @@ var shops_router = module.exports = express.Router();
 var _ = require("underscore");
 var co = require("co");
 var path = require("path");
+
+var mongoose = require("mongoose");
+var Japaname = mongoose.model("Japaname");
 var Artwork = require("../models/artworks.js");
 
 shops_router.get("/:shopname/",(req,res,next)=>{
   co(function*(){
     var shopname = req.params.shopname;
+    var japaname_code = req.query.japaname;
+
+
+    if(japaname_code && Japaname.isJapanameCode(japaname_code)){
+      var japaname = yield Japaname.findByCode(japaname_code).exec();
+    }
 
     var artworks = Artwork.getList();
 
-    var arton_artwork = _(artworks).filter((a)=>a.artwork_name != "risumaru_ofuda");
+    var arton_artwork = _(artworks).filter(
+      (a)=>a.artwork_name != "risumaru_ofuda");
 
 
     if(shopname !== "arton"){  //haveRight = authModule.authorize("arton")
       return next();//not found
     }
 
-    res.render("shops",{artworks:arton_artwork});
+    res.render("shops/shop-arton",{
+      artworks:arton_artwork,
+      japaname
+    });
   }).catch((err)=>{
     return next(err);
   });
