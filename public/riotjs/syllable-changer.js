c75ae4b20d5d697e663ed59d6727c5c94fdfdf45
udonka <icymasa@gmail.com>
2017-01-08 00:53:52 +0900

    added animation to syllable change. it seems good.

--- public/riotjs/syllable-changer.js
+++ public/riotjs/syllable-changer.js
@@ -1,5 +1,5 @@
 
-riot.tag2('syllable-changer', '<div class="syllableChanger" if="{opened}" riot-style="top:{y}px; left:{x}px" onclick="{stopPropagation}"> <p class="syllableChanger__sentence">Change syllable separation?</p> <ul> <li each="{syllable in syllable_path}">{syllable.romaji}<span each="{kanji in syllable.atejis.slice(0,3)}">{kanji.kanji}</span></li> </ul> <div class="syllableChanger__buttonsContainer"> <button class="syllableChanger__changeButton" onclick="{OKPushed}">OK</button> <button class="syllableChanger__cancelButton" onclick="{hide}">Cancel</button> </div> </div>', '', '', function(opts) {
+riot.tag2('syllable-changer', '<div class="syllableChanger" if="{opened}" riot-style="top:{y}px; left:{x}px" onclick="{OKPushed}"> <p class="syllableChanger__sentence">Change syllable separation?</p> <ul> <li each="{syllable in syllable_path}">{syllable.romaji}<span each="{kanji in syllable.atejis.slice(0,3)}">{kanji.kanji}</span></li> </ul> <div class="syllableChanger__buttonsContainer"> <button class="syllableChanger__changeButton" onclick="{OKPushed}">OK</button> <button class="syllableChanger__cancelButton" onclick="{hide}">Cancel</button> </div> </div>', '', '', function(opts) {
     var this_tag = this;
     this.from_syllable = opts.from_syllable;
     this.syllable_path = opts.syllable_path;
@@ -41,9 +41,7 @@ riot.tag2('syllable-changer', '<div class="syllableChanger" if="{opened}" riot-s
       if(this.onOK && typeof this.onOK ==="function"){
         this.onOK(this.syllable_path);
       }
-    }.bind(this)
 
-    this.stopPropagation = function(e){
       e.stopPropagation();
     }.bind(this)
 });
